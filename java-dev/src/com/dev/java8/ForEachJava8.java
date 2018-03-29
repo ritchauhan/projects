050ac28ecfcfa1817f9b9a68da0f8b7cfbe559c8
Ani Ranjan <ritchauhan@gmail.com>
2018-03-29 08:40:14 +0530

    java8 features demo package

---
+++ java-dev/src/com/dev/java8/ForEachJava8.java
@@ -0,0 +1,20 @@
+package com.dev.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author USER
+ * Class to demo forEach method in java 8 with Iterable interface 
+ *
+ */
+public class ForEachJava8 {
+	//test main methods
+	public static void main(String args[]) {
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i=0; i<10; i++) {
+			list.add(i);
+		}
+	}
+}
