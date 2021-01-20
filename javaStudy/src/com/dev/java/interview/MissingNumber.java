2a7d8f3791bd5b1e32c34b427107aa9cdbe252af
ritchauhan <ritchauhan@gmail.com>
2021-01-20 09:56:16 +0530

    Interview prep

---
+++ javaStudy/src/com/dev/java/interview/MissingNumber.java
@@ -0,0 +1,19 @@
+package com.dev.java.interview;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Find the missing no in int array.
+ * total length is equal to array size and numbers are unique
+ */
+public class MissingNumber {
+    public static void main(String[] args) {
+
+    }
+
+    static int getMissingNo(int[] nums) {
+
+        return 0;
+    }
+}
--- latestJava/src/com/dev/java/basic/newSwitch.java
+++ latestJava/src/com/dev/java/basic/newSwitch.java
@@ -1,2 +1,17 @@
-package com.dev.java.basic;public class newSwitch {
+package com.dev.java.basic;
+
+public class newSwitch {
+
+    public static void main(String[] args) {
+        System.out.println(print(1));
+    }
+
+    static String print(int n) {
+        return switch (n) {
+            case 0 -> "zero";
+            case 1 -> "one";
+            case 2 -> "two";
+            default -> "default";
+        };
+    }
 }
