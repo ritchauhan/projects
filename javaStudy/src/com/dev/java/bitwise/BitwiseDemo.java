02edf3da459a66a42e5f724cd668ade72daa641f
ritchauhan <ritchauhan@gmail.com>
2021-03-08 10:28:12 +0530

    Bitwise updated

--- javaStudy/src/com/dev/java/bitwise/BitwiseDemo.java
+++ javaStudy/src/com/dev/java/bitwise/BitwiseDemo.java
@@ -6,6 +6,8 @@ public class BitwiseDemo {
         int a = 5; // 00000101
         int b = 7; // 00000111
         int c = 42;
+        int d = 128; // 01000000
+        int e = -128; // 111101000000
 
         // And (00000101)
         System.out.println(a & b); // 5
@@ -16,10 +18,27 @@ public class BitwiseDemo {
         // Xor (00000010)
         System.out.println(a ^ b); // 2
 
-        //
+        // 2 compliment
         System.out.println(~a); // -6
         System.out.println(~b); // -8
         System.out.println(~c); // -43 need to understand this
+
+        // left shift
+        System.out.println(d<<2); // 512 - 0000 0001 0000 0000 (128 * 2 * 2)
+        System.out.println(e<<2); // -512 - 1111 0001 0000 0000 (-128 * 2 * 2)
+
+        // right shift
+        System.out.println(d>>2); // 32 ((128/2)/2)
+        System.out.println(e>>2); // -32 - ((128/2)/2) it preserve the signed bit
+
+        // unsigned right shift
+        System.out.println(d>>>2); // 32 right shift by 2 and 0 on left since d=128
+        System.out.println(e>>>2); // 1073741792 0011 1111 1111 1111 1111 1111 0010 0000
+
+        // 2^30 power 
+        System.out.println(Math.pow(2, 30)); // 1.073741824E9
+
+
 
     }
 }
