699ffc287368fa84a7752c64ea7a370dfcba2693
ritchauhan <ritchauhan@gmail.com>
2021-03-30 21:25:39 +0530

    level order tree print

---
+++ javaStudy/src/com/dev/java/dataStructure/tree/LevelOrderPrintWithQueue.java
@@ -0,0 +1,17 @@
+package com.dev.java.dataStructure.tree;
+
+/**
+ * level order print with new line
+ */
+public class LevelOrderPrintWithQueue {
+    /**
+     * Level by level tree traversal
+     */
+    public static void printTreeByLevel(Node tree) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
