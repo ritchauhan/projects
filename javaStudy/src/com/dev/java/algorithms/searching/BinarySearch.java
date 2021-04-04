58108a3a000cb1d423557f0f756019e78fbb8f19
ritchauhan <ritchauhan@gmail.com>
2021-04-04 12:00:48 +0530

    searching and sorting

--- javaStudy/src/com/dev/java/algorithms/searching/BinarySearch.java
+++ javaStudy/src/com/dev/java/algorithms/searching/BinarySearch.java
@@ -4,6 +4,7 @@ package com.dev.java.algorithms.searching;
  * class to demo binary search algorithms
  * Very important - input should always sorted then only we can user binary search
  * this will have complexity O(log n) since we don't need to traverse each node.
+ * // need to check for recursive binary search
  *
  */
 public class BinarySearch {
@@ -26,13 +27,28 @@ public class BinarySearch {
     }
 
     //TODO
-    private static boolean recursiveSearch() {
-        return true;
+    private static boolean recursiveSearch(int[] arr, int start, int end, int searchVal) {
+        System.out.println(start);
+        System.out.println(end);
+        if (end >= start && end <= arr.length-1) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == searchVal) {
+                return true;
+            }
+            if (arr[mid] < searchVal) {
+                return recursiveSearch(arr, start, mid - 1, searchVal);
+            }
+
+            return recursiveSearch(arr, mid + 1, end, searchVal);
+
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 5, 10, 12, 14, 15};
-        System.out.println(iterativeSearch(arr, 9));
+//        System.out.println(iterativeSearch(arr, 10));
+        System.out.println(recursiveSearch(arr, 0, arr.length-1, 12 ));
     }
 
 }
---
+++ javaStudy/src/com/dev/java/algorithms/sorting/MergeSort.java
@@ -0,0 +1,10 @@
+package com.dev.java.algorithms.sorting;
+
+/**
+ * Merge sort demo class
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+
+    }
+}
