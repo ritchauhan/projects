0c3452d042e0a9da74baf5e23845678f8c4a58cc
ritchauhan <ritchauhan@gmail.com>
2021-04-12 08:01:45 +0530

    singleton implementation

--- javaStudy/src/com/dev/java/interview/designPattern/singleton/SingletonBasic.java
+++ javaStudy/src/com/dev/java/interview/designPattern/singleton/SingletonBasic.java
@@ -10,16 +10,48 @@ import java.lang.reflect.InvocationTargetException;
 public class SingletonBasic implements Serializable, Cloneable {
     // eager loading
     // private static SingletonBasic singletonBasic = new SingletonBasic();
-    private static SingletonBasic singletonBasic = null;
+    private static volatile SingletonBasic singletonBasic = null;
     private SingletonBasic() {
+        // issue 1 fix: reflection
+//        if (singletonBasic != null) {
+//            throw new RuntimeException("Can not create object here get from instance method ");
+//        }
         System.out.println("Creating the instance: ");
     }
 
     public static SingletonBasic getInstance() {
         // lazy loading
+        // double check singleton implementation
         if (singletonBasic == null) {
-            singletonBasic = new SingletonBasic();
+            // Thread issue fix for singleton
+            // need to make singletonBasic volatile as well
+            synchronized (SingletonBasic.class) {
+                if (singletonBasic == null) {
+                    singletonBasic = new SingletonBasic();
+                }
+            }
         }
         return singletonBasic;
+    }
+
+//    public static SingletonBasic getInstance() {
+//        return Holder.INSTANCE;
+//    }
+//
+     // Holder pattern
+//    static class Holder {
+//        static final SingletonBasic INSTANCE = new SingletonBasic();
+//    }
+
+    // issue 2 fix : serialization
+    private Object readResolve() throws ObjectStreamException {
+        System.out.println("read resolve ");
+        return singletonBasic;
+    }
+
+    // issue 3 fix : clone
+    @Override
+    protected Object clone() throws CloneNotSupportedException{
+        throw new CloneNotSupportedException();
     }
 }
--- javaStudy/src/com/dev/java/interview/designPattern/singleton/TestClass.java
+++ javaStudy/src/com/dev/java/interview/designPattern/singleton/TestClass.java
@@ -3,17 +3,30 @@ package com.dev.java.interview.designPattern.singleton;
 import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+enum Singleton {
+    INSTANCE;
+}
 
 public class TestClass {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, IOException, CloneNotSupportedException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
         SingletonBasic sb1 = SingletonBasic.getInstance();
         SingletonBasic sb2 = SingletonBasic.getInstance();
+
+        // Based on enum implementation
+//        Singleton sb9 = Singleton.INSTANCE;
+//        Singleton sb10 = Singleton.INSTANCE;
+//        System.out.println(sb9.hashCode());
+//        System.out.println(sb10.hashCode());
 
         // printing hashcode
         printHashCode("sb1", sb1); // 1389647288
         printHashCode("sb2", sb2); // 1389647288
 
         // Drawbacks 1: Reflection issue
+        // If we this before getInstance() method again new object will be created.
         Class sb3 = Class.forName("com.dev.java.interview.designPattern.singleton.SingletonBasic");
         Constructor<SingletonBasic> constructor = sb3.getDeclaredConstructor();
         constructor.setAccessible(true);
@@ -27,15 +40,30 @@ public class TestClass {
         // reading object from a file
         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("obj.ser"));
         SingletonBasic sb5 = (SingletonBasic) objectInputStream.readObject();
-        // printHashCode("sb5", sb5);
+         printHashCode("sb5", sb5);
 
         // Drawback 3: Cloning issue
-        // SingletonBasic sb6 = (SingletonBasic) sb2.clone();
-        // printHashCode("sb6", sb6);
+        // Fix this issue with throwing clone not supported exception or override clone and return existing object
+        try {
+            SingletonBasic sb6 = (SingletonBasic) sb2.clone();
+            printHashCode("sb6", sb6);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
 
+        // Drawback 4: thread issue
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(TestClass::useSingleton);
+        executorService.submit(TestClass::useSingleton);
+        executorService.shutdown();
     }
 
     private static void printHashCode(String obj, SingletonBasic singletonBasic) {
         System.out.println(obj + " Hashcode : " + singletonBasic.hashCode());
+    }
+
+    private static void useSingleton() {
+        SingletonBasic s1 = SingletonBasic.getInstance();
+        printHashCode("Singleton", s1);
     }
 }
